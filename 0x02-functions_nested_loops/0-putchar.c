a7d24e8cce0a64f2d9127ee147c39b12173d1c3e
EricLenus <ledamaeric@gmail.com>
2022-10-19 13:25:51 -0700

    task 00

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
- * main - _putchar
+ * main - print _putchar,
  *
- * Description: prints _putchar
- *
- * Return: 0
+ * Return:0
  */
+
 int main(void)
 {
-	char school[10] = "_putchar";
-
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(school[i]);
-	}
-	putchar(10);
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
