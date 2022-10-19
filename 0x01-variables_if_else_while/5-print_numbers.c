c0eec1ef67a4e6836fad6d7f5b8084f7352df0d8
EricLenus <ledamaeric@gmail.com>
2022-10-19 09:33:35 -0700

    task 05

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		++digit;
+	}
+	printf("\n");
+	return (0);
+}
