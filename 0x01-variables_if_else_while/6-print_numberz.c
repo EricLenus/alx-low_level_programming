ea690ee59c0e94e84602b4af5cf8113d2577a303
EricLenus <ledamaeric@gmail.com>
2022-10-19 09:37:43 -0700

    task 06

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - numberz
+ *
+ * return: 0
+ */
+int main(void)
+{
+	int (digit <= 9)
+	{
+		putchar(digit + '0');
+		++digit;
+	}
+	putchar('\n');
+	return (0);
+}
