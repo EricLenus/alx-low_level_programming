4619dd6726a666cd816ee14e5dab14de18cd8ca2
EricLenus <ledamaeric@gmail.com>
2022-10-21 11:39:40 -0700

    task 00

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - tests function that ptints if integer is positive or negative 
+ * main - tests function that ptints if integer is positive or negative
  *
  * Description: Test function
  *
