ee668b9fa86b476beaf694ab8208e034d5044e55
EricLenus <ledamaeric@gmail.com>
2022-10-24 05:16:37 -0700

    task 02

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * larget_number - return the largets 
+ * @a: first integer
+ * @b: second interger
+ * @c: third integer
+ * Return: largest number 
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	
+	return (largest);
+}
