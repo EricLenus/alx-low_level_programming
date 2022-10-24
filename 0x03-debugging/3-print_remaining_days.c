cc4f5287276023a8058330d620da6d523d251bcf
EricLenus <ledamaeric@gmail.com>
2022-10-24 11:23:22 -0700

    task 03

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -5,6 +5,7 @@
  * @month: month in number format
  * @day: day of month
  * @year: year
+ * Description: Leap year
  * Return: void
  */
 void print_remaining_days(int month, int day, int year)
@@ -16,18 +17,18 @@ void print_remaining_days(int month, int day, int year)
 			day++;
 		}
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 -day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
-	else 
+	else
 	{
 		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02/%02d/%04d\n", month, day - 31, year);
-	}
-	else
-	{
+		}
+		else
+		{
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 -day);
+		printf("Remaining days: %d\n", 365 - day);
+		}
 	}
-	} 
 }
