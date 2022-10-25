110924aa3636909fd95f71e43dea70d9a2898598
EricLenus <ledamaeric@gmail.com>
2022-10-25 11:12:38 -0700

    task 05

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * rev_string - reverses a string 
+ * rev_string - reverses a string
  * @s: input
  * Return: string in reverse
  */
 void rev_string(char *s)
 {
-	char rv = s [0];
+	char rv = s[0];
 	int c = 0;
 	int i;
 
