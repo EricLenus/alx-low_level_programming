48ec595132d8bda663d2cc821847d159a990f75a
EricLenus <ledamaeric@gmail.com>
2022-10-30 14:11:50 -0700

    task 03

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_number - print from 0 to 9
+ * print_numbers - print from 0 to 9
  * Return: Always 0
  */
-void print_number(void)
+void print_numbers(void)
 {
 	int a = 0;
 
