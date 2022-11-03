a85a6b6f689c9409c32795277b27f4ad913e1127
EricLenus <ledamaeric@gmail.com>
2022-11-03 08:47:25 -0700

    task 02

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,18 +2,19 @@
 #define MAIN_H
 
 /**
- * File: - main.h
- *
- * Dec: header containing prototypes
+ * void prototypes
+ * int prototype
+ * char prototype
  */
+
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int _putchar(char c);
 
 #endif
