7fd39488fa338a27134bc3b07d63884cda80a4d2
EricLenus <ledamaeric@gmail.com>
2022-11-08 00:56:29 -0800

    task 00

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	if (size != 0)
 	{
 		array = (char *)malloc(size * sizeof(char));
-		if array != (NULL)
+		if (array != NULL)
 		{
 			for (i = 0; i < size; i++)
 				array[i] = c;
