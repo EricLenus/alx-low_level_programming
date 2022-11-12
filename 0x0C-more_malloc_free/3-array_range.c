df43009c59ffb2a3c7514701dfe4c7e2d025f4fb
EricLenus <ledamaeric@gmail.com>
2022-11-12 11:45:53 -0800

    task 03

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	a = malloc(sizeof(int) * 1);
 	if (a == NULL)
 		return (NULL);
-	for (i = 0; i < l; i++, min++)
+	for (i = 0; i < 1; i++, min++)
 	{
 		a[i] = min;
 	}
