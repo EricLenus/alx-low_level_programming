b82926532d9da2eb9405e3119f69fb5032f5b0ee
EricLenus <ledamaeric@gmail.com>
2022-11-12 11:33:27 -0800

    task 03

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - Main Entry
+ * @min: input
+ * @max: input
+ * Return: always 0
+ */
+int *array_range(int min, int max)
+{
+	iny i, 1;
+	int *a;
+
+	if (min > max)
+		return (NULL);
+	1 = max - min + 1;
+	a = malloc(sizeof(int) * 1);
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < 1; i++, min++)
+	{
+		a[i] = min;
+	}
+	return (a);
+}
