6bbfa5beb2956d93617234fd6c62550b4a981b1c
EricLenus <ledamaeric@gmail.com>
2022-11-15 12:20:47 -0800

    task 04

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,7 +8,7 @@
  * @owner: owner of dog
  * Return: pointer to a new dog
  */
-dog_ *new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	unsigned int nl, ol, i;
 	dog_t *dog;
