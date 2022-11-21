c40ef758235553b669cdffbd0ca03db4d7649caf
EricLenus <ledamaeric@gmail.com>
2022-11-20 23:35:35 -0800

    task 03

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,7 +14,12 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		ext(98);
+		exit(98);
+	}
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
 	}
 	operation = get_op_func(argv[2]);
 
@@ -23,7 +28,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
