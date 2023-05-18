056c2cb3dc1715c779308a369469bdc6ef05be63
EricLenus <ledamaeric@gmail.com>
2023-05-18 15:18:35 -0700

    task 06

---
+++ 6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a program that prints various computer types 
+ * return 0 (success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("size of char: %lu bytes(s)\n", (unsigned long)sizeof(a));
