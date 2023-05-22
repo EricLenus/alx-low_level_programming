fab12565d6752521a9e458242ad93085f498d6d9
EricLenus <ledamaeric@gmail.com>
2023-05-22 12:39:09 -0700

    task 06

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,13 @@
  * main - Entry
  * Return: Always 0
  */
-int main(void)
+int main()
 {
+	int  intType;
+	float floatType;
+	double doubleType;
+	char CharType;
+
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("size of a long int: %i byte(s)\n", sizeof(long int));
