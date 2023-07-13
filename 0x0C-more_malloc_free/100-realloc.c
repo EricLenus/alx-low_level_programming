80befade7871909ee749e3fd153dcbfa0ecacfed
EricLenus <ledamaeric.com>
2023-07-13 13:34:22 -0700

    task advanced

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * _realoc - reallocates a memory block using malloc and free
- * @ptr: pointer to previous memory allocated by malloc
- * @old_size: size of memory allocated for pointer
- * @new_size: new size of memory block
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
  *
- * Return: newly allocated pointer to memory block
+ * Return: pointer to the newly allocated memory block
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
@@ -16,27 +17,34 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
+
 	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
+
 	if (!ptr)
 		return (malloc(new_size));
+
 	ptr1 = malloc(new_size);
 	if (!ptr1)
 		return (NULL);
+
 	old_ptr = ptr;
+
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
 			ptr1[i] = old_ptr[i];
 	}
+
 	if (new_size > old_size)
 	{
-		for (i = 0l i < old_size; i++)
+		for (i = 0; i < old_size; i++)
 			ptr1[i] = old_ptr[i];
 	}
+
 	free(ptr);
 	return (ptr1);
 }
