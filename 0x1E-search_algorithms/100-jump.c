8a8542726275727db74ad6e73fbee5ab1fd9b7ab
EricLenus <ericledama@gmail.com>
2024-05-08 05:53:13 -0700

    Jump && git push " clear git add . && git commit -m Jump

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,46 +2,43 @@
 #include <math.h>
 
 /**
-*jump_search - searches for a value in a sorted array of integers
-*               using the Jump search algorithm
-*@array: pointer to the first element of the array to search in
-*@size: number of elements in array
-*@value: value in array to be searched for
-*Return: first index where 'value' is located
-*/
-
+ * jump_search - searches for a value in an array of
+ * integers using the Jump search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t prev = 0, step;
+	int index, m, k, prev;
 
-	step = sqrt(size);
 	if (array == NULL || size == 0)
 		return (-1);
-	while (array[(int)fmin(step, size) - 1] < value)
+
+	m = (int)sqrt((double)size);
+	k = 0;
+	prev = index = 0;
+
+	do {
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+			return (index);
+		k++;
+		prev = index;
+		index = k * m;
+	} while (index < (int)size && array[index] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+	for (; prev <= index && prev < (int)size; prev++)
 	{
-		printf("Value checked array[%f] = [%d]\n", fmin(step,
- size) - 1, array[(int)fmin(step, size) - 1]);
-		prev = step;
-		step += sqrt(size);
-		if (prev >= size)
-			return (-1);
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
 	}
 
-	while (array[prev] < value)
-	{
-		prev++;
-
-		 /*If we reached next block or end of
-		 array, element is not present*/
-		if (prev == fmin(step, size))
-			return (-1);
-		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
-	}
-	/*If element is found*/
-	if (array[prev] == value)
-	{
-		printf("Value checked array[%lu] = [%u]\n", prev, array[prev]);
-		return prev;
-	}
 	return (-1);
 }
